37e8a907dfa005991e76805e3b205e80f794e60f
leejooy96 <leejooy96@gmail.com>
2022-12-04 15:51:50 +0900

    rewrite typescript

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # create-retron
 
-See: https://github.com/jooy2/retron
+This is a `npm` only project for easy project configuration with `npm init`.
+
+- The main project link is here: https://github.com/jooy2/retron
 
 ## Installation
 
---
+++ lib/index.ts
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+import { spawn } from 'child_process';
+import { existsSync, rmSync, statSync } from 'fs';
+import { join } from 'path';
+
+const PKG_NAME = 'retron';
+const PKG_AUTHOR = 'jooy2';
+
+async function run(): Promise<void> {
+	const clone = async (): Promise<number> =>
+		new Promise((resolve, reject) => {
+			try {
+				spawn(
+					'git',
+					['clone', `https://github.com/${PKG_AUTHOR}/${PKG_NAME}`, PKG_NAME, '--depth', '1'],
+					{ stdio: 'inherit' }
+				)
+					.on('error', (err) => {
+						reject(err);
+					})
+					.on('exit', (code) => {
+						resolve(code);
+					});
+			} catch (e) {
+				reject(e);
+			}
+		});
+
+	const installModules = async (): Promise<number> =>
+		new Promise((resolve, reject) => {
+			try {
+				const npmCommand = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
+				spawn(npmCommand, ['install'], { stdio: 'inherit', cwd: join(process.cwd(), PKG_NAME) })
+					.on('error', (err) => {
+						reject(err);
+					})
+					.on('exit', (code) => {
+						resolve(code);
+					});
+			} catch (e) {
+				reject(e);
+			}
+		});
+
+	try {
+		if (existsSync(PKG_NAME) && statSync(PKG_NAME).isDirectory()) {
+			console.error(`Failed: Directory "${PKG_NAME}" already exists.`);
+			process.exit(1);
+			return;
+		}
+
+		await clone();
+
+		// Remove files used only in git repositories
+		rmSync(join(process.cwd(), PKG_NAME, '.git'), { recursive: true, force: true });
+
+		await installModules();
+	} catch (e) {
+		console.error(e?.message);
+		process.exit(1);
+	}
+}
+
+run();
